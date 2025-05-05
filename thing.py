4531dc4ffe81b45a11cad23c2e151f68f9596ccd
Alexander Mitrokhin <=>
2025-05-06 01:50:49 +0200

    volume fix

--- thing.py
+++ thing.py
@@ -6,8 +6,9 @@ import numpy as np
 
 A4_FREQUENCY = 440.0
 BPM = 180.0
-# AMPLITUDE=1
+AMPLITUDE=0.03
 
+SQUARE_FRACTION = 0.6
 
 
 if __name__ == "__main__":
@@ -15,19 +16,21 @@ if __name__ == "__main__":
     musical_notes = read_file_to_list("examples/sunday-clothes.txt")
     physical_notes = read_notes(musical_notes, A4_FREQUENCY, BPM)
     
-    wave_generator = Square()
-    wave_generator2 = Sinusoidal()
+    wave_generator = Sinusoidal()
+    wave_generator2 = Square()
 
     samples = []
     for note in physical_notes:
         new_note = wave_generator.generate_samples(note.frequency, note.duration)
         samples.extend(new_note)
-    sample_arr = np.array(samples)
+    sinusoudal_arr = np.array(samples)
     
     samples2 = []
     for note in physical_notes:
         new_note = wave_generator2.generate_samples(note.frequency, note.duration)
         samples2.extend(new_note)
-    sample_arr += np.array(samples2)
+    square_arr = np.array(samples2)
 
-    wavfile.write("sunday-clothes.wav", wave_generator.sample_frequency, sample_arr)
+    sample_arr = (1 - SQUARE_FRACTION) * sinusoudal_arr + SQUARE_FRACTION * square_arr
+
+    wavfile.write("sunday-clothes.wav", wave_generator.sample_frequency, AMPLITUDE * sample_arr)
